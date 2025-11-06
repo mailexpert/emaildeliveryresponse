3d875646614f31263b7bd38b031276cae60635bc
Mail Xpert <mailxpert@emaildelivery.space>
2025-11-06 01:05:34 -0500

    updated

--- rsync/rsync-github-to-prod.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gitdir="emaildeliveryresponse"
-devdir="emaildelivery"
-cd "/home/mailxpert/github/"+$gitdir
-
-rsync -ravu --progress --exclude-from='rsync/rsync-exclude-toprod.txt' /home/mailxpert/github/$gitdir/ /var/www/$devdir/
